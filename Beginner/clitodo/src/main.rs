f48d3a09a9d3f4f4b3a042b2fdef588e2f3f2fee
wlmr-rk <wlmr.rk@gmail.com>
2025-06-07 14:35:24 +0800

    moved readme to correct location part 2

---
+++ Beginner/clitodo/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let name = "Rust Dev";
+    let country = "Philippines";
+    let age = 28;
+
+    println!("I am a {} from {}", name, country);
+    println!("I am {} years old", age);
+    println!("In hexadecimal, my age is {:x}", age);
+    println!("In binary, my age is {:b}", age);
+    println!("My name has {} characters", name.len());
+}
+
+//Today I will be learning about Rust's formatting capabilities.
+//Day 1: Basic Formatting
+//I will create a simple program that prints my name, country, and age.
+////I will also explore different formatting options like hexadecimal and binary representations of
+///my age.
+/////I will also learn how to get the length of a string in Rust.
+/////I will use the println! macro for output and format specifiers like {:x} for hexadecimal and
+///{:b} for binary.
+//////I will also learn how to use the len() method to get the length of a string.
+/////I will run the program to see the output and ensure everything works as expected.
+/////I will also explore how to use comments in Rust to document my code.
